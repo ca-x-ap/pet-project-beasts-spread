0c593d9246dea60ace8f8d4adced1b4e4bcad255
Alexander Poz <iaks.fine@gmail.com>
2023-01-23 00:41:01 +0300

    changes

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
 const spread = ({ maxLiveTime, spreadCountTimes }) => {
   var beasts = { live: [[], []], death: [[], []] };
   var getNum = num => Math.round(Math.random(num));
-  var unixTime = +new Date();
+  var unixTime = 0;
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     var numberOfBeastList = getNum(1);
     beasts.live[numberOfBeastList].push({
       id: beasts.live[numberOfBeastList].length,
       id0: null,
       id1: null,
-      unixTime: unixTime + maxLiveTime,
+      unixTime: unixTime + getNum(maxLiveTime) + 1,
     });
   }
 
-  for (let spreadCount = 0; spreadCount < spreadCountTimes; spreadCount++) {
+  for (let spreadCount = 0; spreadCount < spreadCountTimes /*|| beasts.live[0].length === 0 || beasts.live[1].length === 0 */; spreadCount++) {
+    unixTime++;
+
     for (var i = 0; i < beasts.live[1].length; i++) {
+      if (beasts.live[1][i] === undefined) continue;
       if (beasts.live[1][i].unixTime < unixTime) {
-        beasts.death[1].push(beasts.live[1][i]);
-        delete beasts.live[1][i];
+        // beasts.death[1].push(beasts.live[1][i]);
+        beasts.live[1].splice(i, 1)
       }
     }
 
     var beastsLive_1Copy = JSON.parse(JSON.stringify(beasts.live[1]));
 
-    for (var i = 0; i < beasts.live[0].length; i++) {
-      const sda = beasts.live[0][i].unixTime;
-      if (beasts.live[0][i].unixTime < unixTime) {
-        beasts.death[0].push(beasts.live[0][i]);
-        delete beasts.live[0][i];
+    for (var index = 0; index < beasts.live[0].length; index++) {
+      if (beasts.live[0][index] === undefined || beasts.live[0][index] === null) continue;
+      if (beasts.live[0][index].unixTime < unixTime) {
+        // beasts.death[0].push(beasts.live[0][index]);
+        beasts.live[0].splice(index, 1)
+        index--;
         continue;
       }
 
       var partnerNum = getNum(beastsLive_1Copy.length - 1);
       var partnerBeast = beastsLive_1Copy[partnerNum];
-      if (partnerBeast === undefined) continue;
+      if (partnerBeast === undefined || partnerBeast === null) continue;
       delete beastsLive_1Copy[partnerNum];
 
-      var countOfChildren = getNum(3);
+      var countOfChildren = getNum(3) + 1;
 
       for (let j = 0; j < countOfChildren; j++) {
-        var numberOfBeastList = getNum(1);
+        var numberOfBeastList = getNum(2);
         beasts.live[numberOfBeastList].push({
           id: beasts.live[numberOfBeastList].length,
-          id0: beasts.live[0][i].id,
+          id0: beasts.live[0][index].id,
           id1: partnerBeast.id,
-          unixTime: unixTime + getNum(maxLiveTime),
+          unixTime: unixTime + getNum(maxLiveTime) + 1,
         });
       }
     }
@@ -53,4 +57,7 @@ const spread = ({ maxLiveTime, spreadCountTimes }) => {
   return beasts;
 }
 
-console.dir(spread({ maxLiveTime: 1, spreadCountTimes: 1000 }));
+const beasts = spread({ maxLiveTime: 5, spreadCountTimes: 10 });
+console.group('Beasts');
+console.table(beasts);
+console.groupEnd('Beasts');
