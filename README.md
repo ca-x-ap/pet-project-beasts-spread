8c4835d6a71d5c19d85a75225c42992226d6e710
Alexander Poz <iaks.fine@gmail.com>
2023-01-26 20:08:25 +0300

    add readme comment

---
+++ README.md
@@ -0,0 +1,16 @@
+# Beasts tree
+
+To build and run rust run:
+
+```
+rs_build.sh
+rs_run.sh
+```
+
+To build and run bin rust run:
+
+```
+rs_build_bin.sh file_name
+```
+
+For view web open or run index.html
--- rs/src/beasts.rs
+++ rs/src/beasts.rs
@@ -1,39 +1,39 @@
 fn main() {
     struct Animal {
-      id: u32,
-      father_id: Option<u32>,
-      mather_id: Option<u32>,
-      birthday: String,
-  }
+        id: u32,
+        father_id: Option<u32>,
+        mather_id: Option<u32>,
+        birthday: String,
+    }
 
-  impl Animal {
-      fn spread(&self) {
-          // logic to spread animals on rust goes here 
-      } 
-  }
+    impl Animal {
+        fn spread(&self) {
+            // logic to spread animals on rust goes here
+        }
+    }
 
-  let mut count = 0u32;
+    let mut count = 0u32;
 
-  println!("Let's count until infinity!");
+    println!("Let's count until infinity!");
 
-  // Infinite loop
-  loop {
-      count += 1;
+    // Infinite loop
+    loop {
+        count += 1;
 
-      if count == 3 {
-          println!("three");
+        if count == 3 {
+            println!("three");
 
-          // Skip the rest of this iteration
-          continue;
-      }
+            // Skip the rest of this iteration
+            continue;
+        }
 
-      println!("{}", count);
+        println!("{}", count);
 
-      if count == 5 {
-          println!("OK, that's enough");
+        if count == 5 {
+            println!("OK, that's enough");
 
-          // Exit this loop
-          break;
-      }
-  }
+            // Exit this loop
+            break;
+        }
+    }
 }
