070d7fb5900baa71fb187c3b32fc4fc4a3a2dfe9
Alexander Poz <iaks.fine@gmail.com>
2023-01-26 23:23:29 +0300

    changes

--- rs/src/beasts.rs
+++ rs/src/beasts.rs
@@ -1,39 +1,47 @@
 fn main() {
-    struct Animal {
-        id: u32,
-        father_id: Option<u32>,
-        mather_id: Option<u32>,
-        birthday: String,
-    }
+    // let base_beast = (id, id_pa, id_ma, birthday);
+		// for i in 0..xs.len() + 1 { // OOPS, one element too far
 
-    impl Animal {
-        fn spread(&self) {
-            // logic to spread animals on rust goes here
-        }
-    }
+    // fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    //     // `let` can be used to bind the members of a tuple to variables
+    //     let (int_param, bool_param) = pair;
+    //     (bool_param, int_param)
+    // }
+    // struct Animal {
+    //     id: u32,
+    //     father_id: Option<u32>,
+    //     mather_id: Option<u32>,
+    //     birthday: String,
+    // }
 
-    let mut count = 0u32;
+    // impl Animal {
+    //     fn spread(&self) {
+    //         // logic to spread animals on rust goes here
+    //     }
+    // }
 
-    println!("Let's count until infinity!");
+    // let mut count = 0u32;
+
+    // println!("Let's count until infinity!");
 
     // Infinite loop
-    loop {
-        count += 1;
+    // loop {
+    //     count += 1;
 
-        if count == 3 {
-            println!("three");
+    //     if count == 3 {
+    //         println!("three");
 
-            // Skip the rest of this iteration
-            continue;
-        }
+    //         // Skip the rest of this iteration
+    //         continue;
+    //     }
 
-        println!("{}", count);
+    //     println!("{}", count);
 
-        if count == 5 {
-            println!("OK, that's enough");
+    //     if count == 5 {
+    //         println!("OK, that's enough");
 
-            // Exit this loop
-            break;
-        }
-    }
+    //         // Exit this loop
+    //         break;
+    //     }
+    // }
 }
