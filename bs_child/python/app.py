3a2af76567407dcaeaf0c88b8c5d90ce68eb8a6a
Alexander Poz <iaks.fine@gmail.com>
2023-02-01 19:26:56 +0300

    commit mesage

--- bs_child/python/app.py
+++ bs_child/python/app.py
@@ -1,6 +1,58 @@
+import atexit
+import sys
+
+@atexit.register
+def end_func():
+	print('> END <\n')
+
 def on_startup():
 	from utils.spread import spread_beasts
-	spread_beasts()
+
+	# for x in fruits:
+	#   print(x)
+	# print(beasts)
+	# print(beasts.live)
+	# print(beasts.death)
+	# print(beasts.live[0])
+	# print(beasts.live[1])
+
+	years = 0
+	min_live_time = 0
+	live_time = 3
+	start_count = 20
+
+	len_sys_argv = len(sys.argv) - 1
+	print(f'Starting { sys.argv[0] }')
+	if len_sys_argv == 0 or len_sys_argv == 1:
+		print('Need wrinte next arguments:')
+		print('(You can run app as "app.py years_count(0)(need write) start_count(20) live_time(3) min_live_time(0)" )')
+
+	if len_sys_argv >= 1:
+		years = int(sys.argv[1])
+	else:
+		years = int(input('Years count (can be esasd): '))
+
+	if len_sys_argv >= 2:
+		start_count = int(sys.argv[2])
+
+	if len_sys_argv >= 3:
+		live_time = int(sys.argv[3])
+
+	if len_sys_argv >= 4:
+		min_live_time = int(sys.argv[4])
+
+	print(f"""
+		years_count: { years }
+		start_count: { start_count }
+		live_time: { live_time }
+		min_live_time: { min_live_time }
+		""")
+
+	beasts = spread_beasts(min_live_time, live_time, start_count, years)
+	print('live ' + str(len(beasts[0][0])) + str(len(beasts[0][1])))
+	print('death ' + str(len(beasts[1][0])) + str(len(beasts[1][1])))
 
 if __name__ == '__main__':
+	print('\n> START <')
+
 	on_startup()
--- bs_child/python/utils/spread.py
+++ bs_child/python/utils/spread.py
@@ -1,4 +1,4 @@
-def spread_beasts():
+def spread_beasts(min_live_time, live_time, start_count, years):
 		# import time
 		# from random import randrange, random
 		# import string
@@ -8,13 +8,15 @@ def spread_beasts():
 		import random, string
 
 		time = 2023
-		live_time = 3
 		beasts = [[[],[]],[[],[]]]
+
 		def randomword(length):
 			letters = string.ascii_lowercase
 			return ''.join(random.choice(letters) for i in range(length))
+
 		index = 0
-		while index < 20:
+		start_count = start_count / 2
+		while index < start_count:
 			beasts[0][0].append({
 				'id': len(beasts[0][0]),
 				'id_0': None,
@@ -31,36 +33,33 @@ def spread_beasts():
 			})
 			index += 1
 
-
 		# for index, element in enumerate(beasts):
-		for count in range(3):
+		for count in range(years):
 			time += count
 			random.shuffle(beasts[0][0])
 			# random.shuffle(beasts[0][1])
 			for beast0, beast1 in zip(beasts[0][0], beasts[0][1]):
 				list_number = random.randint(0, 1)
+				# gen = ''
+				# if list_number == 0:
+					# beast0['gen'].split(" ")[0] + ' ' + beast1['gen'].split(" ")[1]
+				# else:
+					# beast1['gen'].split(" ")[1] + ' ' + beast0['gen'].split(" ")[0]
+
 				beasts[0][list_number].append({
 					'id': len(beasts[0][list_number]),
 					'id_0': beast0['id'],
 					'id_1': beast1['id'],
 					'birthday': time,
+					# 'gen': gen
 					'gen': beast0['gen'].split(" ")[0] + ' ' + beast1['gen'].split(" ")[1]
 				})
 
-				if beast0['birthday'] + random.randint(0, live_time) < time:
+				if beast0['birthday'] + random.randint(min_live_time, live_time) < time:
 					beasts[1][0].append(beast0)
 					del beast0
-				if beast1['birthday'] + random.randint(0, live_time) < time:
+				if beast1['birthday'] + random.randint(min_live_time, live_time) < time:
 					beasts[1][1].append(beast1)
 					del beast1
 
-		print('live ' + str(len(beasts[0][0])) + str(len(beasts[0][1])))
-		print('death ' + str(len(beasts[1][0])) + str(len(beasts[1][1])))
-
-		# for x in fruits:
-		#   print(x)
-		# print(beasts)
-		# print(beasts.live)
-		# print(beasts.death)
-		# print(beasts.live[0])
-		# print(beasts.live[1])
+		return beasts
